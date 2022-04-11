a43d8742553f9bb78e933acf1bb8efc54e379208
SaimHasanD <saimhasan514@gmail.com>
2022-04-12 00:29:10 +0600

    color ,button

--- src/App.js
+++ src/App.js
@@ -11,18 +11,41 @@ function App() {
     setUser(UserData);
   }, [])
 
+  const [cart, setCart] = useState([]);
+
+  const handleAddUser = (users) => {
+    const newCart = [...cart, users];
+    setCart(newCart);
+  }
+
+  // let totalIncome = 0;
+  // for (let i = 0; i < cart.length; i++) {
+  //   const user = cart[i];
+  //   totalIncome = totalIncome + Number( user.yearlyIncome);
+  // }
+  const totalIncome = cart.reduce((totalIncome, cart) => totalIncome + Number(cart.yearlyIncome), 0) 
+
   return (
     <div className="App">
-      <h1>Total User {user.length}</h1>
-      <div id="user">
-      <div id="userParent">
-        {
-          user.map(user => <User user={user} key={user.id}>  </User>)
-        }
+        <div id="addedInfo">
+        <h1>Total Added Friends: {cart.length}</h1>
+      <h1>Total yearly Income: ${totalIncome}</h1>
+      <div>
+        Friends Name
+      {
+        cart.map((cart) => <li key={cart.index}>{cart.name}</li> )
+      }
+        </div>
       </div>
+      <div id="user">
+        <div id="userParent">
+          {
+            user.map(user => <User user={user} handleAddUser={handleAddUser} key={user.id}>  </User>)
+          }
+        </div>
 
       </div>
-     
+
     </div>
   );
 }
--- src/component/User/User.js
+++ src/component/User/User.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './user.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRegular, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 const User = (props) => {
 
-    console.log(props.user);
+    // console.log(props.user);
     const { name, username, image, gender, phone, email, city, yearlyIncome} = props.user;
+    const handleAddUser = props.handleAddUser;
 
     return (
         <div >
             <article id="users">
                 <img src={image} alt="" />
-                <p className="nameHover">Full Name <small>(User Name) </small></p>
                 <h1 className="name">{name} <small>({username}) </small></h1>
+                <div className="btnDiv">
+                <button className="btn" onClick={() => handleAddUser(props.user)}>Add <FontAwesomeIcon icon={faPlus} /></button>
+                <button className="btn">Massage</button>
+                </div>
 
+                <h3 id='about'>About</h3>
                 <div id='userInfo'>
                 <h4>Gender: {gender}</h4>
                 <h4>Phone: {phone}</h4>
